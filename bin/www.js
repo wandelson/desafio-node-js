f8aecf2330c58c0e219cc038fefddb7e832422cd
wandelson <wandelson.bezer@gmail.com>
2016-11-01 18:01:18 -0200

    ...

--- bin/www.js
+++ bin/www.js
@@ -1,7 +1,7 @@
 
 var app = require('../app');
 
-app.set('port', process.env.PORT || 8080);
+app.set('port', process.env.PORT || 8000);
 
 var server = app.listen(app.get('port'), function () {
     console.log('Servidor ativo: ' + server.address().port);
