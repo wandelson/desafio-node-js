1c29d17619cddb65afe341ea02a46277135c3d24
wandelson <wandelson.bezer@gmail.com>
2016-11-02 23:34:30 -0200

    ...

--- server.js
+++ server.js
@@ -1,27 +1,29 @@
 var express = require('express');
 var bodyParser = require('body-parser');
-var mongoose = require('mongoose');
-var http = require('http');
+var mongoose =  require('mongoose');
 var config = require('./config/config');
-var routes = require('./routes/routes');
+var routes = require('./routes/routes')
+var http = require('http');
 
 var app = express();
 
-mongoose.connect(config.connectionString);
-var db = mongoose.connection;
+var port = process.env.PORT || 8000; // first change
 
 var server = http.createServer(app);
 
+mongoose.connect(config.connectionString);
+
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.text());
-app.use(bodyParser.json({ type: 'application/json' }));
-app.use('/api', routes);
 
-var port = 8000;
+app.use(bodyParser.urlencoded({
+    extended:false
+}));
 
-server.listen(port, function () {
+app.use('/api',routes);
+
+server.listen(port, function(){
     console.log('Server up and runnning on port ' + port);
 });
+
 
 module.exports = app;
